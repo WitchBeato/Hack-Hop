9b0646f4ed6a8c12b0efa0444b2bdc406e2b6f75
Ali Korkmaz <alisonkorkmaz@gmail.com>
2025-04-27 02:27:05 +0300

    Player Healt HUD added

---
+++ Hack&Hop/Assets/Sctripts/INPCHealt.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface INPCHealt 
+{
+    public void setMaxHP(float value);
+    public void setHP(float value);
+    public void getAttack(float value);
+}
