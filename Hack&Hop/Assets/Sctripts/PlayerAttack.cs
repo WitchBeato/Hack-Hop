299f22c83eb7ad884a624fb4f8428f1ed08e379b
Ali Korkmaz <alisonkorkmaz@gmail.com>
2025-04-29 00:52:10 +0300

    Attack time added to the game

--- Hack&Hop/Assets/Sctripts/PlayerAttack.cs
+++ Hack&Hop/Assets/Sctripts/PlayerAttack.cs
@@ -7,17 +7,21 @@ public class PlayerAttack : MonoBehaviour
 {
     public float phoneVelocity = 2f;
     public float Damage = 10f;
+    public float attackInterval = 0.5f;
     public AudioClip attackClip;
     [SerializeField] private UnityEventList unityEventList;
     [SerializeField] private GameObject attackItem;
     [SerializeField] private GameObject attackLocation;
     private PlayerItemManager playerItemManager;
+    private float lastAttackTime = 0f;
     void Start()
     {
         playerItemManager = GetComponent<PlayerItemManager>();
     }
     public void Attack(){
         if(!playerItemManager.isPhoneAvaible) return;
+        if(lastAttackTime >= Time.time) return;
+        lastAttackTime = Time.time +attackInterval;
         GameObject newPhone = Instantiate(attackItem, attackLocation.transform.position, Quaternion.identity);
         if(newPhone.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody)){
             int val = 1;
